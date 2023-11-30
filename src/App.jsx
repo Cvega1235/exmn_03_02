e63f2fe44bcd1616a325419419e35a28db33f833
root <kikivega2002@gmail.com>
2023-11-30 11:55:20 -0400

    Componente Dashboard realizado

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Components/Home'
 import About from './Components/About'
+import Dashboard from './Components/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <About/>
+      <Dashboard/>
     </>
   )
 }
